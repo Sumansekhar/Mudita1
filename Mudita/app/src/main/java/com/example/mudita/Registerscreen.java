be3fcfe93a53cbe2edad3cea909bf6a10eecf722
Suman Sekhar <sumansekhar2866@gmail.com>
2020-04-03 02:21:06 +0530

    Splashscreen(without graphics) Loginscreen done(without forgot password) Registrationscreen Trying Google Sign in

--- Mudita/app/src/main/java/com/example/mudita/Registerscreen.java
+++ Mudita/app/src/main/java/com/example/mudita/Registerscreen.java
@@ -1,13 +1,20 @@
 package com.example.mudita;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Registerscreen extends AppCompatActivity {
@@ -17,28 +24,53 @@ public class Registerscreen extends AppCompatActivity {
     private FirebaseAuth auth;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registerscreen);
-        Name=findViewById(R.id.name);
-        Email=findViewById(R.id.emailenter);
-        Password=findViewById(R.id.passwordenter);
-        Age=findViewById(R.id.age);
-        Register=findViewById(R.id.registerbutton);
-        Sign_in=findViewById(R.id.signintransparent);
-        Male=findViewById(R.id.maleicon);
-        Female=findViewById(R.id.femaleicon);
-        auth=FirebaseAuth.getInstance();
+        Name = findViewById(R.id.name);
+        Email = findViewById(R.id.email);
+        Password = findViewById(R.id.passwordreg);
+        Age = findViewById(R.id.age);
+        Register =findViewById(R.id.registerbutton);
+        Sign_in = findViewById(R.id.signintransparent);
+        Male = findViewById(R.id.maleicon);
+        Female = findViewById(R.id.femaleicon);
+        auth = FirebaseAuth.getInstance();
         Register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String txt_name=Name.getText().toString();
-                String txt_email=Email.getText().toString();
-                String txt_password=Password.getText().toString();
-                String txt_Email=Email.getText().toString();
+                String txt_name = Name.getText().toString();
+                String txt_email = Email.getText().toString();
+                String txt_password = Password.getText().toString();
+                String txt_age = Age.getText().toString();
+                if ((TextUtils.isEmpty(txt_name)) || (TextUtils.isEmpty(txt_email)) || (TextUtils.isEmpty(txt_password)) || (TextUtils.isEmpty(txt_age)))
+                {
+                    Toast.makeText(Registerscreen.this, "Empty Credentials!", Toast.LENGTH_SHORT).show();
+                } else if (txt_password.length() < 6)
+                {
+                    Toast.makeText(Registerscreen.this, "Password should be greater than 6 character.", Toast.LENGTH_SHORT).show();
+                } else
+                    {
+                    Registeruser(txt_name, txt_email, txt_password, txt_age);
+                    }
+
             }
-        };
+        });
+    }
+
+    private void Registeruser(String name, String email, String password, String age) {
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(Registerscreen.this,new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {Intent intent=new Intent(Registerscreen.this,Mainscreen1.class);
+                startActivity(intent);
+                finish();}
+                else
+                {Toast.makeText(Registerscreen.this,"Registration Failed",Toast.LENGTH_SHORT).show();}
+
+            }
+        });
     }
 }
